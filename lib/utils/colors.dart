23b14cf25e217b85869a31c2dd074e176b3e221f
Felix Cary Giovanni <felix.cary@gmail.com>
2023-11-22 13:42:13 +0700

    change background color of copilot

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -8,3 +8,4 @@ const kShowCaseNeutral1000 = Color(0xFF000000);
 const kShowCaseNeutral800 = Color(0xFF424242);
 const kShowCaseNeutral300 = Color(0xFFE0E0E0);
 const kShowCaseNeutral0 = Color(0xFFFFFFFF);
+const kV2Neutral800 = Color(0xFF282828);
